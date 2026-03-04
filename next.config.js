8c419ea5f6907cdb66afcec978f68586f707968e
Divyanshusaini55 <22f2000900@ds.study.iitm.ac.in>
2026-03-04 23:01:06 +0530

    fix css path for github pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
+  output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
