6aeaa4264c833a7ac8b1a82d20b86035e045d0a5
Divyanshusaini55 <22f2000900@ds.study.iitm.ac.in>
2026-05-28 02:04:03 +0530

    Stop directory

--- .next/build/postcss.js
+++
@@ -1,6 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
-R.c("chunks/[turbopack-node]_transforms_postcss_ts_13a19914._.js")
-R.c("chunks/[root-of-the-server]__a5d921a4._.js")
-R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
-R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Downloads/Portfolio/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
-module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Downloads/Portfolio/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++
@@ -1,1 +0,0 @@
-0:{"buildId":"U7qBRwYMbd0sWm_19w95q","tree":{"name":"","paramType":null,"paramKey":"","hasRuntimePrefetch":false,"slots":{"children":{"name":"__PAGE__","paramType":null,"paramKey":"__PAGE__","hasRuntimePrefetch":false,"slots":null,"isRootLayout":false}},"isRootLayout":false},"staleTime":300}
